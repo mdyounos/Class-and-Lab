fd001ad0e92b0f84e9a09cfbf8e6946fd1f147ab
Md Younos <82257632+mdyounos@users.noreply.github.com>
2021-06-22 06:48:22 +0600

    Overloading Binary Operator
    Overloading Binary Operator using constructor.

---
+++ Overloading Binary Operator.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+class Distance{
+    int feet;
+    float inches;
+public:
+    Distance()
+    {
+        feet = 0;
+        inches = 0.0;
+    }
+    Distance operator+(Distance d2) const
+    {
+        int f = feet+d2.feet;
+        float i = inches + d2.inches;
+        if(i>=12.0)
+        {
+            i-=12.0;
+            f++;
+        }
+        return Distance(f,i);
+    }
+    Distance(int f, int i)
+    {
+        feet = f;
+        inches = i;
+    }
+    void showdist()
+    {
+        cout<< feet <<"feet ";
+        cout<< inches << "inches "<<endl;
+    }
+    void getdist()
+    {
+        cout<<"Enter feet: ";
+        cin>>feet;
+        cout<<"Enter inches: ";
+        cin>>inches;
+    }
+};
+int main()
+{
+    Distance dist1,dist3;
+    dist1.getdist();
+    Distance dist2(11,6.25);
+    dist3=dist1+dist2;
+    cout<<"dist1=";dist1.showdist();cout<<endl;
+    cout<<"dist2=";dist2.showdist();cout<<endl;
+    cout<<"dist3=";dist3.showdist();cout<<endl;
+}
